caa7c20fcc714aacd2e96e55cc0c16beadc05ed7
todd-estes <elestes@gmail.com>
2020-11-10 08:10:11 -0700

    Make final route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-
   resources :hospitals do
   end
-
-  # delete '/appointments/:doctor_id/:patient_id', to: 'appointments#destroy'
-
 
   resources :doctors, only: :show do
     resources :appointments, only: :destroy
